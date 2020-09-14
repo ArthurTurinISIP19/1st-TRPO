9f00fe74983fab1256c7ba1acc597627998de956
Tyurin <ezeka1n@mail.ru>
2020-09-14 11:59:58 +0900

    1 commit

---
+++ Solve_TRPO_!/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Solve_TRPO__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Х килло конфет стоит У ");
+            double n;
+            double m;
+
+            double x;
+            double a;
+            double y;
+            double k;
+
+            Console.WriteLine("Ведите кол-во килло.");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ведите цену за этот вес.");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сколько вам надо конфет.");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сколько хотите потратить.");
+            k = Convert.ToDouble(Console.ReadLine());
+
+            m = y/x * a;
+            n = k / a * x;
+            Console.WriteLine();
+            Console.WriteLine("Сколько потратите = " + m);
+
+            Console.WriteLine("Сколько получите = " + n);
+
+            Console.ReadKey();
+        }
+    }
+}
